dc8471c11bf56f0799a0d407be82ce7eab3cb5df
WijnaldumDenver <denver.wijnaldum.24@example.com>
2024-09-25 12:42:49 -0300

    style: update sidebar buttons for improved UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "@mantine/core/styles.css";
 import Provider from "../util/Providers";
 import { FirebaseNextJSProvider } from "firebase-nextjs/client/auth";
+import { MantineProvider } from "@mantine/core";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,9 +32,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <FirebaseNextJSProvider>
-          <Provider>{children}</Provider>
-        </FirebaseNextJSProvider>
+        <MantineProvider>
+          <FirebaseNextJSProvider>
+            <Provider>{children}</Provider>
+          </FirebaseNextJSProvider>
+        </MantineProvider>
       </body>
     </html>
   );
