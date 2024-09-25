fb72d87e60d9450f1082074c403a5193fab147d7
WijnaldumDenver <denver.wijnaldum.24@example.com>
2024-09-25 13:00:43 -0300

    refactor(sidebar): render buttons from array

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,48 +4,44 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 import { Button, UnstyledButton } from "@mantine/core";
 
 export default function Sidebar() {
+  const sideBarButtons = [
+    {
+      icon: "clarity:tasks-line",
+      label: "Tasks",
+    },
+    {
+      icon: "carbon:settings",
+      label: "Settings",
+    },
+    {
+      icon: "ic:round-logout",
+      label: "Log Out",
+    },
+  ];
   return (
-    <>
-      <div className="fixed flex flex-col bg-clip-border rounded-r-xl bg-white text-gray-700 min-h-screen w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-        <div className="mb-2 p-4 mx-auto flex flex-col justify-center ">
-          <h5 className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-gray-900">
-            Tasks Manager
-          </h5>
-          <div className="min-w-16 mx-auto mt-8">
-            <ProfileButton size={128} />
-          </div>
-        </div>
-        <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
-          <SideBarButton icon={"fa-solid:tasks"} label="Tasks" />
-          <LogoutButton>
-            <SideBarButton icon="ic:round-logout" label="Log Out" />
-          </LogoutButton>
-        </nav>
-      </div>
-
-      <div className="w-full pt-5 px-4 mb-8 mx-auto ">
-        <div className="text-sm text-gray-700 py-1">
-          Made with{" "}
-          <a
-            className="text-gray-700 font-semibold"
-            href="https://www.material-tailwind.com/docs/react/sidebar?ref=tailwindcomponents"
-            target="_blank"
-          >
-            Material Tailwind
-          </a>{" "}
-          by{" "}
-          <a
-            href="https://www.creative-tim.com?ref=tailwindcomponents"
-            className="text-gray-700 font-semibold"
-            target="_blank"
-          >
-            {" "}
-            Creative Tim
-          </a>
-          .
+    <div className="fixed flex flex-col bg-clip-border rounded-r-xl bg-white text-gray-700 min-h-screen w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+      <div className="mb-2 p-4 mx-auto flex flex-col justify-center ">
+        <h5 className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-gray-900">
+          Tasks Manager
+        </h5>
+        <div className="min-w-16 mx-auto mt-8">
+          <ProfileButton size={128} />
         </div>
       </div>
-    </>
+      <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
+        {sideBarButtons.map((button) => (
+          <>
+            {button.label === "Log Out" ? (
+              <SideBarButton icon={button.icon} label={button.label} />
+            ) : (
+              <LogoutButton>
+                <SideBarButton icon={button.icon} label={button.label} />
+              </LogoutButton>
+            )}
+          </>
+        ))}
+      </nav>
+    </div>
   );
 }
 
