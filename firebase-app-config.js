d6ca3190211d0e352d4b73b5fa055bbc7b4e7a03
WijnaldumDenver <denver.wijnaldum.24@example.com>
2024-09-27 16:26:53 -0300

    feat: Updated firebase config

--- firebase-app-config.js
+++ firebase-app-config.js
@@ -1,12 +1,18 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 export const firebaseConfig = {
-  "projectId": "tasksmanager-2b34d",
-  "appId": "1:739127811129:web:7620d395797df1de7341e8",
-  "storageBucket": "tasksmanager-2b34d.appspot.com",
-  "apiKey": "AIzaSyBkMA1Gvre88gGIE7ALFD_abEIj_JS6F5E",
-  "authDomain": "tasksmanager-2b34d.firebaseapp.com",
-  "messagingSenderId": "739127811129"
-}
+  projectId: "tasksmanager-2b34d",
+  appId: "1:739127811129:web:7620d395797df1de7341e8",
+  storageBucket: "tasksmanager-2b34d.appspot.com",
+  apiKey: "AIzaSyBkMA1Gvre88gGIE7ALFD_abEIj_JS6F5E",
+  authDomain: "tasksmanager-2b34d.firebaseapp.com",
+  messagingSenderId: "739127811129",
+};
 
-export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseApp = getApps().length
+  ? getApp()
+  : initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
