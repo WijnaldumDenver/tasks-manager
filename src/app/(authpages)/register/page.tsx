4bc11b038c0af10bedbe25e2df10cf6802a7ead3
WijnaldumDenver <denver.wijnaldum.24@example.com>
2024-09-29 01:38:32 -0300

    chore: Updated auth design

--- src/app/(authpages)/register/page.tsx
+++ src/app/(authpages)/register/page.tsx
@@ -23,8 +23,8 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="mx-auto max-w-md space-y-6 h-screen  bg-white text-black flex flex-col justify-center items-center transition-all p-2">
-      <div className="flex flex-col shadow-lg border rounded-xl w-full bg-gray-100/40">
+    <div className="space-y-6 h-screen w-screen bg-white text-black flex flex-col justify-center items-center transition-all p-2">
+      <div className="max-w-md flex flex-col shadow-lg border rounded-xl w-full bg-gray-100/40">
         <div className="flex flex-col border-b rounded-xl w-full  p-4 md:p-8 lg:p-10 py-10 items-center bg-white">
           {!currentUser ? (
             <>
